dd05930a19ea8a1900528b24982f5fa0c175186b
Anni <anni.alanen@metropolia.fi>
2025-11-16 22:27:51 +0200

    fixausta

---
+++ fetch.js
@@ -0,0 +1,69 @@
+export async function fetchData(url, options) {
+    try{
+        const response = await fetch(url, options);
+
+        if(!response.ok) {
+            throw new Error(`HTTP error ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+export async function getRestaurants() {
+    try{
+        const url = 'https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants'; 
+        const options =  {
+       method: 'GET',
+     }
+
+     const restaurants = await fetchData(url, options);
+     return restaurants;
+
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+export async function getRestaurantById(id) {
+    try{
+        const url = `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/${id}`;
+        const options = {
+            method: 'GET',
+        }
+        const restaurant = await fetchData(url, options);
+        return restaurant;
+    }
+    catch(error) {
+        console.log(error);
+    }    
+}
+
+
+
+
+
+export async function sendUserData() {       //käyttäjätunnusta varten
+ try {
+     const user = {
+       username : 'example.user',  ///tähä käyttäjä ja salasana
+       passwork : 'salasana'
+     };
+     const url = 'https://media2.edu.metropolia.fi/restaurant/api/v1/auth/login';
+     const options = {
+       method: 'POST',
+       headers: {
+           'Content-Type': 'application/json',
+       },
+       body: JSON.stringify(user)
+     }
+     const userData = await fetchData(url, options);
+     console.log('data:', userData);
+   } catch (error) {
+     console.error('An error occurred:', error);
+   }
+}  
--- index.js
+++ index.js
@@ -1,108 +1,76 @@
+import {fetchData, getRestaurants, getRestaurantById, sendUserData} from './fetch.js';
+
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementsByClassName('search-input');
 const modal = document.getElementById("myModal");
 const span = document.getElementsByClassName("close")[0];
 
-async function fetchData(url, options) {
-    try{
-        const response = await fetch(url, options);
 
-        if(!response.ok) {
-            throw new Error(`HTTP error ${response.status}`);
-        }
-        const data = await response.json();
-        return data;
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
-
-async function getRestaurants() {
-    try{
-        const url = 'https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants'; 
-        const options =  {
-       method: 'GET',
-     }
-
-     const restaurantData = await fetchData(url, options);
-     await displayRestaurants(restaurantData);
-
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
 
 async function displayRestaurants(data){
-    console.log(data);
-    data.sort((a, b) => {
-        let x = a.name.toLowerCase();
-        let y = b.name.toLowerCase();
-        if (x < y) {return -1;}
-        if (x > y) {return 1;}
-        return 0;
-    });
+    try {
+        console.log(data);
+        data.sort((a, b) => {
+            let x = a.name.toLowerCase();
+            let y = b.name.toLowerCase();
+            if (x < y) {return -1;}
+            if (x > y) {return 1;}
+            return 0;
+        });
 
 
-    for (let i = 0; i < data.length; i++) {
-        let container = document.getElementById("container");
+        for (let i = 0; i < data.length; i++) {
+            let container = document.getElementById("container");
 
 
-        let box = document.createElement("div");
-        box.className = "box";
-        box.onclick = function() {
-            modal.style.display = "block";
+            let box = document.createElement("div");
+            box.className = "box";
+            box.setAttribute("id", `${data[i]._id}`);
+
+            box.addEventListener("click", event => {
+                modal.style.display = "block";
+                displayRestaurantById(event.target.id)
+            });
+
+            let nameElement = document.createElement("h2");
+            let cityElement = document.createElement("p");
+            let addressElement = document.createElement("p");
+
+            let nameText = document.createTextNode(data[i].name);
+            let cityText = document.createTextNode(data[i].city);
+            let addressText = document.createTextNode(data[i].address);
+        
+
+            nameElement.appendChild(nameText);
+            cityElement.appendChild(cityText);
+            addressElement.appendChild(addressText);
+
+            box.appendChild(nameElement);
+            box.appendChild(cityElement);
+            box.appendChild(addressElement);
+            container.appendChild(box);
         }
-
-        let nameElement = document.createElement("h3");
-        let cityElement = document.createElement("p");
-        let addressElement = document.createElement("p");
-
-        let nameText = document.createTextNode(data[i].name);
-        let cityText = document.createTextNode(data[i].city);
-        let addressText = document.createTextNode(data[i].address);
-    
-
-        nameElement.appendChild(nameText);
-        cityElement.appendChild(cityText);
-        addressElement.appendChild(addressText);
-
-        box.appendChild(nameElement);
-        box.appendChild(cityElement);
-        box.appendChild(addressElement);
-        container.appendChild(box);
-   }
+    }
+    catch(error) {
+        console.log(error);
+    }
+    return data;
 
 }
 
 
-async function restaurantById(id) {
-    modal.style.display = "block";
+async function displayRestaurantById(id) {
+    console.log(id);
+
+    let resData = await getRestaurantById(id);
+    let restaurantId = document.createElement("p");
+    let testi = document.createTextNode(id);
+
+    //restaurantById.appendChild(testi);
+
+
     
 }
-
-
-async function sendUserData() {       //käyttäjätunnusta varten
- try {
-     const user = {
-       username : 'example.user',  ///tähä käyttäjä ja salasana
-       passwork : 'salasana'
-     };
-     const url = 'https://media2.edu.metropolia.fi/restaurant/api/v1/auth/login';
-     const options = {
-       method: 'POST',
-       headers: {
-           'Content-Type': 'application/json',
-       },
-       body: JSON.stringify(user)
-     }
-     const userData = await fetchData(url, options);
-     console.log('data:', userData);
-   } catch (error) {
-     console.error('An error occurred:', error);
-   }
-}  
 
 
 
@@ -117,4 +85,6 @@ window.onclick = function(event) {
     
 }
 
-getRestaurants();
+
+const restaurantData = await getRestaurants();
+const data = await displayRestaurants(restaurantData);
